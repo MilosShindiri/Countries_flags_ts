c8fa81b287416e330a5d61a9937d96d682c611e9
unknown <milos.petrovic@shindiristudio.com>
2025-08-01 09:40:24 +0200

    all done

--- dist/filtering.js
+++ dist/filtering.js
@@ -1,6 +1,6 @@
-import { allCountriesData, renderCountries } from './renderCountries.js';
-let searchInput = document.getElementById('input');
-let filtering = document.getElementById('region_filter');
+import { allCountriesData, renderCountries } from "./renderCountries.js";
+let searchInput = document.getElementById("input");
+let filtering = document.getElementById("region_filter");
 // DEBOUNCING
 function debounce(func, delay) {
     let timeoutId;
@@ -15,13 +15,13 @@ function debounce(func, delay) {
 export function applyFilters() {
     let query = searchInput === null || searchInput === void 0 ? void 0 : searchInput.value.trim().toLowerCase();
     let region = filtering === null || filtering === void 0 ? void 0 : filtering.value.trim().toLowerCase();
-    let filtered = allCountriesData.filter(country => {
+    let filtered = allCountriesData.filter((country) => {
         let nameMatch = country.name.common.toLowerCase().includes(query);
         let regionMatch = country.region.toLowerCase().includes(region);
         return nameMatch && regionMatch;
     });
     renderCountries(filtered);
 }
-filtering.addEventListener('change', applyFilters);
-searchInput.addEventListener('input', (debounce(applyFilters, 500)));
+filtering.addEventListener("change", applyFilters);
+searchInput.addEventListener("input", debounce(applyFilters, 500));
 //# sourceMappingURL=filtering.js.map
